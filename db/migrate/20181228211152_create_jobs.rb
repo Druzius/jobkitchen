c47bda25af92da2af7796daa524d73bf790935a4
Daniel Kol Adam <venomdrophearthstone@gmail.com>
2018-12-29 00:04:57 +0200

    added more tables to schema and added carrierwave setup

---
+++ db/migrate/20181228211152_create_jobs.rb
@@ -0,0 +1,15 @@
+class CreateJobs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :jobs do |t|
+      t.string :title
+      t.text :description
+      t.string :url
+      t.string :job_type
+      t.string :location
+      t.string :job_author
+      t.string :apply_url
+
+      t.timestamps
+    end
+  end
+end
