6baf95d0e98fee5163571dd8fba9fb148cf608c7
Daniel Kol Adam <venomdrophearthstone@gmail.com>
2019-01-05 14:29:29 +0200

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,94 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Job.destroy_all
+User.destroy_all
+
+
+user = User.new(
+  id: 2,
+  name: "Daniel Kol",
+  email: "daniel@example.com",
+  password: "asdasd",
+  password_confirmation: "asdasd"
+)
+user.save!
+
+Job.create!([{
+               title: "Head Chef",
+               description: "Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Maecenas sed diam eget risus varius blandit sit amet non magna. Aenean lacinia bibendum nulla sed consectetur. Donec sed odio dui. Venenatis dapibus posuere. Nullam id dolor id nibh ultricies vehicula ut id elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur blandit tempus porttitor. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam id dolor id nibh ultricies vehicula ut id elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Nulla vitae elit libero, a pharetra augue. Nulla vitae elit libero, a pharetra augue. Vestibulum id ligula porta felis euismod semper. Maecenas faucibus mollis interdum. Maecenas sed diam eget risus varius blandit sit amet non magna.",
+               url: "https://facebook.com",
+               apply_url: "https://facebook.com/apply",
+               job_type: "Full-time",
+               location: "TLV",
+               job_author: "Aroma",
+               user_id: user.id,
+               avatar: Rails.root.join("app/assets/images/facebook.jpg").open
+             },
+             {
+               title: "Waiter",
+               description: "Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Maecenas sed diam eget risus varius blandit sit amet non magna. Aenean lacinia bibendum nulla sed consectetur. Donec sed odio dui. Venenatis dapibus posuere. Nullam id dolor id nibh ultricies vehicula ut id elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur blandit tempus porttitor. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam id dolor id nibh ultricies vehicula ut id elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Nulla vitae elit libero, a pharetra augue. Nulla vitae elit libero, a pharetra augue. Vestibulum id ligula porta felis euismod semper. Maecenas faucibus mollis interdum. Maecenas sed diam eget risus varius blandit sit amet non magna.",
+               url: "https://slack.com",
+               apply_url: "https://slack.com/apply",
+               job_type: "Contract",
+               location: "TLV",
+               job_author: "Cafe Noir",
+               user_id: user.id,
+               avatar: Rails.root.join("app/assets/images/slack.jpg").open
+             },
+             {
+               title: "Dish Washer",
+               description: "Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Maecenas sed diam eget risus varius blandit sit amet non magna. Aenean lacinia bibendum nulla sed consectetur. Donec sed odio dui. Venenatis dapibus posuere. Nullam id dolor id nibh ultricies vehicula ut id elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur blandit tempus porttitor. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam id dolor id nibh ultricies vehicula ut id elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Nulla vitae elit libero, a pharetra augue. Nulla vitae elit libero, a pharetra augue. Vestibulum id ligula porta felis euismod semper. Maecenas faucibus mollis interdum. Maecenas sed diam eget risus varius blandit sit amet non magna.",
+               url: "https://trello.com",
+               apply_url: "https://trello.com/apply",
+               job_type: "Full-time",
+               location: "Anywhere",
+               job_author: "Aroma",
+               user_id: user.id,
+               avatar: Rails.root.join("app/assets/images/trello.jpg").open
+             },
+             {
+               title: "Host",
+               description: "Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Maecenas sed diam eget risus varius blandit sit amet non magna. Aenean lacinia bibendum nulla sed consectetur. Donec sed odio dui. Venenatis dapibus posuere. Nullam id dolor id nibh ultricies vehicula ut id elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur blandit tempus porttitor. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam id dolor id nibh ultricies vehicula ut id elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Nulla vitae elit libero, a pharetra augue. Nulla vitae elit libero, a pharetra augue. Vestibulum id ligula porta felis euismod semper. Maecenas faucibus mollis interdum. Maecenas sed diam eget risus varius blandit sit amet non magna.",
+               url: "https://wordpress.org",
+               apply_url: "https://wordpress.org/apply",
+               job_type: "Full-time",
+               location: "Anywhere",
+               job_author: "Amore Mio",
+               user_id: user.id,
+               avatar: Rails.root.join("app/assets/images/wordpress.jpg").open
+             },
+             {
+               title: "Waiter",
+               description: "Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Maecenas sed diam eget risus varius blandit sit amet non magna. Aenean lacinia bibendum nulla sed consectetur. Donec sed odio dui. Venenatis dapibus posuere. Nullam id dolor id nibh ultricies vehicula ut id elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur blandit tempus porttitor. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam id dolor id nibh ultricies vehicula ut id elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Nulla vitae elit libero, a pharetra augue. Nulla vitae elit libero, a pharetra augue. Vestibulum id ligula porta felis euismod semper. Maecenas faucibus mollis interdum. Maecenas sed diam eget risus varius blandit sit amet non magna.",
+               url: "https://basecamp.com",
+               apply_url: "https://basecamp.com/apply",
+               job_type: "Full-time",
+               location: "Jerusalem",
+               job_author: "Okinawa",
+               user_id: user.id,
+               avatar: Rails.root.join("app/assets/images/basecamp.jpg").open
+             },
+             {
+               title: "Cook",
+               description: "Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Maecenas sed diam eget risus varius blandit sit amet non magna. Aenean lacinia bibendum nulla sed consectetur. Donec sed odio dui. Venenatis dapibus posuere. Nullam id dolor id nibh ultricies vehicula ut id elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur blandit tempus porttitor. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam id dolor id nibh ultricies vehicula ut id elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Nulla vitae elit libero, a pharetra augue. Nulla vitae elit libero, a pharetra augue. Vestibulum id ligula porta felis euismod semper. Maecenas faucibus mollis interdum. Maecenas sed diam eget risus varius blandit sit amet non magna.",
+               url: "https://twitter.com",
+               apply_url: "https://twitter.com/apply",
+               job_type: "Part-time",
+               location: "Jerusalem",
+               job_author: "Dixie",
+               user_id: user.id,
+               avatar: Rails.root.join("app/assets/images/twitter.jpg").open
+             },
+             {
+               title: "Waiter",
+               description: "Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Maecenas sed diam eget risus varius blandit sit amet non magna. Aenean lacinia bibendum nulla sed consectetur. Donec sed odio dui. Venenatis dapibus posuere. Nullam id dolor id nibh ultricies vehicula ut id elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur blandit tempus porttitor. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam id dolor id nibh ultricies vehicula ut id elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Nulla vitae elit libero, a pharetra augue. Nulla vitae elit libero, a pharetra augue. Vestibulum id ligula porta felis euismod semper. Maecenas faucibus mollis interdum. Maecenas sed diam eget risus varius blandit sit amet non magna.",
+               url: "https://lewagon.com",
+               apply_url: "https://lewagon.com/apply",
+               job_type: "Full-time",
+               location: "TLV",
+               job_author: "Black",
+               user_id: user.id,
+               avatar: Rails.root.join("app/assets/images/web-crunch.jpg").open
+             }
+             ])
