84ec34ad8e55d921a118458a0f288c0a11abd100
Daniel Kol Adam <venomdrophearthstone@gmail.com>
2019-01-06 19:47:50 +0200

    added empty methods

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -1,2 +1,19 @@
 class JobApplicationsController < ApplicationController
+  # def index
+  #     user dashboard for all applications
+  # end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+
 end
