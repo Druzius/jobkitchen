e3389890d8bfd94f018df2b6e9d1372bde9a953b
Daniel Kol Adam <venomdrophearthstone@gmail.com>
2019-01-06 16:40:53 +0200

    added basic validations without formal error message

--- app/models/job.rb
+++ app/models/job.rb
@@ -5,4 +5,10 @@ class Job < ApplicationRecord
   JOB_TYPES = ["Full-time", "Part-time", "Contract"]
 
   validates :title, presence: true
+  validates :apply_url, presence: true
+  validates :job_type, presence: true
+  validates :job_author, presence: true
+  validates :location, presence: true
+  validates :avatar, presence: true
+  validates :description, presence: true
 end
