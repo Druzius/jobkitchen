9b971267fe23fc40ed18133e67c04f4d557210d2
Daniel Kol Adam <venomdrophearthstone@gmail.com>
2019-01-08 13:36:31 +0200

    added controller methodss

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -4,15 +4,24 @@ class JobApplicationsController < ApplicationController
   # end
 
   def new
+    @job = Job.find(params[:job_id])
     @job_applications = JobApplication.new
   end
 
   def create
-
+    @job = Job.find(params[:job_id])
+    @job_applications = JobApplication.new(job_application_params)
+    @job_applications.user = current_user
+    if @job_applications.save!
+      redirect_to jobs_path
+    else
+      render :new
+    end
   end
 
   def destroy
-
+    @job_applications = JobApplication.find(params[:id])
+    @job_application.destroy
+    redirect_to jobs_path
   end
-
 end
