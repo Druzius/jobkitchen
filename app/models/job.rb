45850ea09841be28958e0cbf2d491ae55079c08b
Ruben den Hollander <struben@gmail.com>
2019-01-09 23:33:37 +0200

    adjusting bulma columns

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,7 +3,7 @@ class Job < ApplicationRecord
   has_many :job_applications
   mount_uploader :avatar, AvatarUploader
 
-  JOB_TYPES = ["Full-time", "Part-time", "Contract"]
+  JOB_TYPES = ["כל התחומים", "ייעוץ, מכירות ושיווק", "מקצועות המטבח", "מקצועות כלליים", "מקצועות ניהול ומנהלה", "מקצועות שירות ואירוח"]
 
   validates :title, presence: true
 end
