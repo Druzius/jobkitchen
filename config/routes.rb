23252b9e0d8c938fc2a012f11697bc9e2dae2fb4
Daniel Kol Adam <venomdrophearthstone@gmail.com>
2019-01-21 13:55:47 +0200

    added model and routes and payments controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  get 'payments/index'
+  get 'payments/new'
+  get 'payments/create'
   mount ForestLiana::Engine => '/forest'
+  resources :payments, only: [:index, :new, :create]
   resources :jobs do
     resources :job_applications
   end
