ed22e6832d67d69592d4d8d94eeb942e821592ec
Daniel Kol Adam <venomdrophearthstone@gmail.com>
2019-02-11 16:51:51 +0200

    added api code snippet

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,11 +1,67 @@
+require 'json'
+require "net/http"
+require 'openssl'
 class JobsController < ApplicationController
   before_action :set_job, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
 
+  # register to demo.ezcount.co.il to get your own test keys
+  api_key = '4c4b3fd224e0943891588ea5a70d6cb566af3a5b4d506908ca04b30526234551'
+  api_email = 'demo@ezcount.co.il'
+  # DEVELOPER information, we will notify you for any API problem to this details
+  developer_email = 'danubusiness10@gmail.com'
+  developer_phone = '0508289809'
+
+  url = 'https://demo.ezcount.co.il/api/createDoc'
+
+  data={
+    # CUSTOMER credentials
+    api_key: api_key,
+    api_email: api_email,
+    # developer data
+    developer_email: developer_email,
+    developer_phone: developer_phone,
+    # invoice reciept
+    type: 320,
+    description: "Monthly payment for service",
+    customer_name: "customer name",
+    customer_email: "client@demo.com",
+    customer_address: "Full customer address, city, and house num 42",
+    item:[{
+            catalog_number: "MKT1",
+            details: "item details",
+            amount: 1,
+            price: 255,
+            #this price include the VAT
+            vat_type: "PRE"
+    }],
+    payment:[{
+               # bank transfer
+               payment_type: 4,
+               payment: 255,
+               comment: "transaction number is 23423423"
+    }],
+    # THIS IS A MUST ONLY IN INVOICE RECIEPT
+    price_total: 255,
+    comment: "some general comment for the document",
+  }.to_json
+
+
+
+  uri = URI(url)
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  warn "!!SSL disabled due to a windows bug, https://gist.github.com/luislavena/f064211759ee0f806c88, please do validation in production !!"
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+  req = Net::HTTP::Post.new(uri.path, "Content-Type" => "application/json")
+  req.body = data
+  res = http.request(req)
+  puts "response #{res.body}"
+
   # GET /jobs
   # GET /jobs.json
   def index
-
     if(params.has_key?(:job_type))
       @jobs = Job.where(job_type: params[:job_type]).order("created_at desc")
     elsif(params.has_key?(:location))
