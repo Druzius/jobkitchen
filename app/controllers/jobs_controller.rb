28d159c99091b53dac0a439c626bd58f1ac0c787
Daniel Kol Adam <venomdrophearthstone@gmail.com>
2019-02-14 20:12:11 +0200

    AMIR DA GREATEST, ezcount redirections work without validation (STEPS 1 AND 2)

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,3 +1,9 @@
+require 'net/http'
+require 'uri'
+#on windows env, ruby have problems with verify certificates
+require 'openssl'
+
+
 class JobsController < ApplicationController
   before_action :set_job, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
@@ -32,6 +38,7 @@ class JobsController < ApplicationController
   # POST /jobs
   # POST /jobs.json
   def create
+
     @job = current_user.jobs.build(job_params)
 
     token = params[:stripeToken]
@@ -58,21 +65,82 @@ class JobsController < ApplicationController
     current_user.card_last4 = card_last4
     current_user.save!
 
-    respond_to do |format|
-      if @job.save
-        format.html { redirect_to @job, notice: 'המשרה שלך התפרסמה בהצלחה' }
-        format.json { render :show, status: :created, location: @job }
-      else
-        format.html { render :new }
-        format.json { render json: @job.errors, status: :unprocessable_entity }
-      end
+    # respond_to do |format|
+    if @job.save
+
+      api_key = '4c4b3fd224e0943891588ea5a70d6cb566af3a5b4d506908ca04b30526234551'
+      api_email = 'demo@ezcount.co.il'
+      developer_email = 'DEVELOPER@example.com'
+      url = 'https://demo.ezcount.co.il/api/payment/prepareSafeUrl/clearingFormForWeb'
+
+      data = {
+        api_key: api_key,
+        api_email: api_email,
+        developer_email: developer_email,
+        sum: 5,
+        successUrl: "http://localhost:3000/"
+      }.to_json
+
+      uri = URI url
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      warn "!!SSL disabled due to a windows bug, https://gist.github.com/luislavena/f064211759ee0f806c88, please do validation in production !!"
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      req = Net::HTTP::Post.new(uri.path, "Content-Type" => "application/json")
+      req.body = data
+      res = http.request(req)
+      puts "response #{res.body}"
+
+      response_hash = JSON.parse(res.body)
+      redirect_to response_hash["url"]
+      # token = response_hash.secretTransactionId
+      # raise
+
+      # request = Net::HTTP::Get.new(uri.request_uri)
+      # request.body =  {
+      #   api_key: api_key,
+      #   developer_email: api_email,
+      #   sum: 5,
+      #   successUrl: "http://localhost:3000/"
+      # }.to_json
+      # response = http.request # Net::HTTPResponse object
+      # response.inspect
+      # raise
+
+      # Net::HTTP.start(uri.host, uri.port) do |http|
+      #   request = Net::HTTP::Get.new(uri.request_uri)
+      #   request.body = {api_key: 'f1c85d16fc1acd369a93f0489f4615d93371632d97a9b0a197de6d4dc0da51bf', developer_email: 'venomdrophearthstone@gmail.com', sum: 5, successUrl: "http://localhost:3000/"}.to_json
+
+      #   user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36'
+      #   request.initialize_http_header({"User-Agent" => user_agent})
+
+      #   response = http.request(request)
+      #   puts response.inspect
+      #   raise
+      # end
+
+      # uri = URI('https://demo.ezcount.co.il/api/payment/prepareSafeUrl/clearingFormForWeb')
+      # req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
+      # req.body = {api_key: 'f1c85d16fc1acd369a93f0489f4615d93371632d97a9b0a197de6d4dc0da51bf', developer_email: 'venomdrophearthstone@gmail.com', sum: 5, successUrl: "http://localhost:3000/"}.to_json
+      # res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+      #   http.request(req)
+      #   raise
+      # end
+
+      # format.html { redirect_to @job, notice: 'המשרה שלך התפרסמה בהצלחה' }
+      # format.json { render :show, status: :created, location: @job }
+    else
+      format.html { render :new }
+      format.json { render json: @job.errors, status: :unprocessable_entity }
     end
-
-
-  rescue Stripe::CardError => e
-    flash.alert = e.message
-    render action: :new
   end
+
+
+  # rescue Stripe::CardError => e
+  #   flash.alert = e.message
+  #   render action: :new
+  # end
 
   # PATCH/PUT /jobs/1
   # PATCH/PUT /jobs/1.json
