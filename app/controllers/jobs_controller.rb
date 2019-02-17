0db6fb234571f9476321ef339da603563021f75f
Daniel Kol Adam <venomdrophearthstone@gmail.com>
2019-02-17 15:21:36 +0200

    added ezcount code

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,3 +1,9 @@
+require 'net/http'
+require 'uri'
+#on windows env, ruby have problems with verify certificates
+require 'openssl'
+
+
 class JobsController < ApplicationController
   before_action :set_job, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
@@ -32,6 +38,7 @@ class JobsController < ApplicationController
   # POST /jobs
   # POST /jobs.json
   def create
+
     @job = current_user.jobs.build(job_params)
 
     token = params[:stripeToken]
@@ -58,21 +65,49 @@ class JobsController < ApplicationController
     current_user.card_last4 = card_last4
     current_user.save!
 
-    respond_to do |format|
-      if @job.save
-        format.html { redirect_to @job, notice: 'המשרה שלך התפרסמה בהצלחה' }
-        format.json { render :show, status: :created, location: @job }
-      else
-        format.html { render :new }
-        format.json { render json: @job.errors, status: :unprocessable_entity }
-      end
+    # respond_to do |format|
+    if @job.save
+
+      api_key = '4c4b3fd224e0943891588ea5a70d6cb566af3a5b4d506908ca04b30526234551'
+      api_email = 'demo@ezcount.co.il'
+      developer_email = 'DEVELOPER@example.com'
+      url = 'https://demo.ezcount.co.il/api/payment/prepareSafeUrl/clearingFormForWeb'
+
+      data = {
+        api_key: api_key,
+        api_email: api_email,
+        developer_email: developer_email,
+        sum: 5,
+        successUrl: "http://localhost:3000/"
+      }.to_json
+
+      uri = URI url
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      warn "!!SSL disabled due to a windows bug, https://gist.github.com/luislavena/f064211759ee0f806c88, please do validation in production !!"
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      req = Net::HTTP::Post.new(uri.path, "Content-Type" => "application/json")
+      req.body = data
+      res = http.request(req)
+      puts "response #{res.body}"
+
+      response_hash = JSON.parse(res.body)
+      redirect_to response_hash["url"]
+
+      # format.html { redirect_to @job, notice: 'המשרה שלך התפרסמה בהצלחה' }
+      # format.json { render :show, status: :created, location: @job }
+    else
+      format.html { render :new }
+      format.json { render json: @job.errors, status: :unprocessable_entity }
     end
-
-
-  rescue Stripe::CardError => e
-    flash.alert = e.message
-    render action: :new
   end
+
+
+  # rescue Stripe::CardError => e
+  #   flash.alert = e.message
+  #   render action: :new
+  # end
 
   # PATCH/PUT /jobs/1
   # PATCH/PUT /jobs/1.json
