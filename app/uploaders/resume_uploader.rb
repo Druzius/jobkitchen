ca5fe07b39f91e6843ec1186dc57c0acf144e3cf
Ruben den Hollander <struben@gmail.com>
2019-02-28 17:16:32 +0200

    extension_whitelist

--- app/uploaders/resume_uploader.rb
+++ app/uploaders/resume_uploader.rb
@@ -1,7 +1,7 @@
 class ResumeUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
-  def extension_white_list
+  def extension_whitelist
     %w(pdf doc htm html docx)
   end
 end
