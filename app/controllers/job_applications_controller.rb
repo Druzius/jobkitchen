8adbb3cf721f6d345e5bba61e2ec18e2efe74831
Ruben den Hollander <struben@gmail.com>
2019-03-04 17:11:03 +0200

    added notification after job application

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -14,7 +14,7 @@ class JobApplicationsController < ApplicationController
     @job_application.job = @job
     # @job_application.user = current_user
     if @job_application.save!
-      redirect_to jobs_path
+      redirect_to jobs_path, notice: 'תודה! פנייתך נשלחה בהצלחה'
     else
       render :new
     end
