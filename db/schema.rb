6d4e355a0ffaaaa3b3b097563ad65c65848012e1
Daniel Kol Adam <venomdrophearthstone@gmail.com>
2019-03-05 19:11:52 +0200

    changed stripe schema and some remnants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_04_185132) do
+ActiveRecord::Schema.define(version: 2019_03_05_170521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,12 +70,6 @@ ActiveRecord::Schema.define(version: 2019_03_04_185132) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "stripe_id"
-    t.string "card_brand"
-    t.string "card_last4"
-    t.string "card_exp_month"
-    t.string "card_exp_year"
-    t.datetime "expires_at"
     t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
