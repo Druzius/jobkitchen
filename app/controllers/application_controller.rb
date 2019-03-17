8ec3aa4a4053b8c025bbba1defde089a09dee1dc
Daniel Kol Adam <venomdrophearthstone@gmail.com>
2019-03-17 18:55:40 +0200

    commented potential problematic code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,14 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def default_url_options
-    { host: ENV["HOST"] || "localhost:3000" }
-  end
+  # def default_url_options
+  #   { host: ENV["HOST"] || "localhost:3000" }
+  # end
 
   protected
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
