9f85ae6e02717da329f63f2da9e09aae9eb72bac
Ruben den Hollander <struben@gmail.com>
2019-03-19 17:42:05 +0200

    posted in hebrew

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module JobK
     config.application_name = 'job-k'
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.i18n.default_locale = :'he'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
