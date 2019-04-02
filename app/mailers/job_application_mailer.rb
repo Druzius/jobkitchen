e5c37d8ac1661a2908ba48c988d85c819fb74a65
Daniel Kol Adam <venomdrophearthstone@gmail.com>
2019-04-02 20:03:10 +0300

    changed job application mail to hebrew

--- app/mailers/job_application_mailer.rb
+++ app/mailers/job_application_mailer.rb
@@ -2,7 +2,7 @@ class JobApplicationMailer < ApplicationMailer
   def apply(job_application)
     @job_application = job_application  # Instance variable => available in view
 
-    mail(to: @job_application.email, subject: 'Thanks for applying')
+    mail(to: @job_application.email, subject: 'קורות החיים נשלחו בהצלחה')
     # This will render a view in `app/views/user_mailer`!
   end
 end
