74fd18c4f75f4238cf662b7ee0b9e9cb87b425e0
Ruben den Hollander <struben@gmail.com>
2019-04-03 16:24:11 +0300

    email with attachement method not yet working

--- app/mailers/job_application_mailer.rb
+++ app/mailers/job_application_mailer.rb
@@ -5,4 +5,14 @@ class JobApplicationMailer < ApplicationMailer
     mail(to: @job_application.email, subject: 'קורות החיים נשלחו בהצלחה')
     # This will render a view in `app/views/user_mailer`!
   end
+
+  def email_with_attachment
+    @job_application = job_application  # Instance variable => available in view
+
+    if resume.present?
+      attachments['42.jpg'] = File.read("/path/to/file") # Attached file
+    end
+
+    mail(to: @job_application.job_id.user_id.email, subject: 'somebody applied to your job posting')
+  end
 end
