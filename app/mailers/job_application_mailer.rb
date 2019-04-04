811321ed2ac72bc83074b732a0f72e57bc7a863e
Ruben den Hollander <struben@gmail.com>
2019-04-04 20:15:57 +0300

    email with link to resume

--- app/mailers/job_application_mailer.rb
+++ app/mailers/job_application_mailer.rb
@@ -6,12 +6,8 @@ class JobApplicationMailer < ApplicationMailer
     # This will render a view in `app/views/user_mailer`!
   end
 
-  def email_with_attachment
+  def email_with_attachment(job_application)
     @job_application = job_application  # Instance variable => available in view
-
-    if @job_application.resume.present?
-      attachments[0] = File.read(cl_image_tag(@job_application.resume, attachment: true)) # Attached file
-    end
 
     mail(to: @job_application.job.user.email, subject: 'somebody applied to your job posting')
   end
