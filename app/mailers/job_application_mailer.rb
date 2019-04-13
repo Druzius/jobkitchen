d2474cdb0c7cec467f0759f31d40ac7e56e18480
Ruben den Hollander <struben@gmail.com>
2019-04-13 23:28:17 +0300

    emails are updated

--- app/mailers/job_application_mailer.rb
+++ app/mailers/job_application_mailer.rb
@@ -2,7 +2,7 @@ class JobApplicationMailer < ApplicationMailer
   def apply(job_application)
     @job_application = job_application  # Instance variable => available in view
 
-    mail(to: @job_application.email, subject: 'קורות החיים נשלחו בהצלחה')
+    mail(to: @job_application.email, subject: 'קורות החיים שלך נשלחו בהצלחה!')
     # This will render a view in `app/views/user_mailer`!
   end
 
